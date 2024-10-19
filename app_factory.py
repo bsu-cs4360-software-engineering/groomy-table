8c740b129e11c5b81c58ab3330c27ccb6faa33c3
carterleseman <carter.leseman@outlook.com>
2024-10-19 02:31:44 -0500

    added tests

--- app_factory.py
+++ app_factory.py
@@ -5,10 +5,10 @@ from routes.auth import auth as auth_blueprint
 from routes.dash import dash as dash_blueprint
 # from routes.appointment import appts as appts_blueprint
 
-def create_app():
+def create_app(database_uri="sqlite:///app.db"):
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = "TEMP_KEY"
 
--- routes/auth.py
+++ routes/auth.py
@@ -26,6 +26,8 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('dash.dashboard'))
+        else:
+            form.username.errors.append("Invalid username or password.")
         
     return render_template('login.html', form=form)
 
