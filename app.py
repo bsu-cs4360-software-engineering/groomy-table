c5f5f187bef6fbde9d7fc1dbfaa8ac27076e698f
AidGO <ayolsen14@gmail.com>
2024-10-22 15:14:45 -0500

    Create Account
    Added the ability to create your  own account

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from app_factory import create_app
-from login_manager import login_manager, create_first_user
+from login_manager import login_manager
 from database import db
 
 app = create_app()
@@ -12,7 +12,6 @@ login_manager.init_app(app)
 # Create database tables
 with app.app_context():
     db.create_all()
-    create_first_user(db)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app_factory.py
+++ app_factory.py
@@ -4,6 +4,7 @@ from routes.index import main as main_blueprint
 from routes.auth import auth as auth_blueprint
 from routes.dash import dash as dash_blueprint
 from routes.appointment import appts as appts_blueprint
+from routes.new_acc import new_acc as new_acc_blueprint
 
 def create_app(database_uri="sqlite:///app.db"):
     app = Flask(__name__)
@@ -17,5 +18,6 @@ def create_app(database_uri="sqlite:///app.db"):
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(dash_blueprint)
     app.register_blueprint(appts_blueprint)
+    app.register_blueprint(new_acc_blueprint)
 
     return app
