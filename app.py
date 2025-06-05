eab0102964c3e7be4c7936ebcf2845543cfb3e98
Cayden <cayden.odegard@gmail.com>
2025-06-05 17:39:48 -0500

    removed something from app.py to help host on website

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ login_manager.init_app(app)
 with app.app_context():
     db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
